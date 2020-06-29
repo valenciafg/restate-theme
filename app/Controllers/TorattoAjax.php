2c223d097a4c19704cb10ad8d549d2980bdab9f9
Víctor Valencia <valencia6x@gmail.com>
2020-06-29 08:11:25 -0500

    email body format v2

--- app/Controllers/TorattoAjax.php
+++ app/Controllers/TorattoAjax.php
@@ -9,6 +9,7 @@ class TorattoAjax extends Controller
 {
     function toratto_quotation_form() {
         $headers = array('Content-Type: text/html; charset=UTF-8');
+        //
         parse_str($_POST['form'], $form);
         $model = $form['toratto-quotation-form-model'];
         $fullname = $form['fullname'];
@@ -17,15 +18,18 @@ class TorattoAjax extends Controller
         $message = $form['message'];
         $project_id = $form['project_id'];
         //
+        $projectObj = new  App\Controllers\Project();
+        $project = $projectObj->getSingleProject($project_id);
+        //
         $to = "valencia6x@gmail.com";
         $subject = "Cotización ".$model;
-        $body = "<h1>Cotización de departamento</h1>";
-        $body .= "<strong>Nombres:</strong>".$fullname;
-        $body .= "<strong>Télefono:</strong>".$phone;
-        $body .= "<strong>Correo:</strong>".$email;
-        $body .= "<strong>Mensaje:</strong>".$message;
-        $body .= "<strong>Proyecto:</strong>".$project_id;
-        $body .= "<strong>Departamento:</strong>".$project_id;
+        $body = "<h1>Cotización de departamento</h1><br>";
+        $body .= "<strong>Nombres:</strong>".$fullname."<br>";
+        $body .= "<strong>Télefono:</strong>".$phone."<br>";
+        $body .= "<strong>Correo:</strong>".$email."<br>";
+        $body .= "<strong>Mensaje:</strong>".$message."<br>";
+        $body .= "<strong>Proyecto:</strong>".$project['title']."<br>";
+        $body .= "<strong>Departamento:</strong>".$model."<br>";
 
 
         if( wp_mail( $to, $subject , $body, $headers) === FALSE){
