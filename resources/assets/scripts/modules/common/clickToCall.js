200ca789c447368c8aeb9a6fadef4608c0a09ce4
Víctor Valencia <valencia6x@gmail.com>
2020-07-20 22:48:35 -0500

    click to call example

--- resources/assets/scripts/modules/common/clickToCall.js
+++ resources/assets/scripts/modules/common/clickToCall.js
@@ -1,60 +1,35 @@
 import { Web } from 'sip.js';
 
-// Helper function to get an HTML audio element
-function getAudioElement(id) {
-  const el = document.getElementById(id);
-  // console.log(el instanceof HTMLAudioElement)
-  // console.log(el.constructor.name);
-  if (!(el instanceof HTMLAudioElement)) {
-    // throw new Error(`Element "${id}" not found or not an audio element.`);
-    console.log(`Element "${id}" not found or not an audio element.`);
-  }
-  return el;
-}
-
-// Options for SimpleUser
-const options = {
-  aor: 'sip:110@pbx.cinusual.com', // caller
-  media: {
-    constraints: { audio: true, video: false }, // audio only call
-    remote: { audio: getAudioElement('remoteAudio') }, // play remote audio
-  },
-  userAgentOptions: {
-    authorizationPassword: 'gt123456',
-    authorizationUsername: '110',
-  },
-};
-
 // WebSocket server to connect with
-const server = 'wss://pbx.cinusual.com:7443';
+const server = 'wss://pbx.grupotoratto.com:7443';
 
-// Construct a SimpleUser instance
-
+let simpleUser;
 
 // Connect to server and place call
 
-
-// export default {
-//   initCall() {
-//     simpleUser.connect()
-//       .then(() => simpleUser.call('sip:100@pbx.cinusual.com'))
-//       .catch((error) => {
-//         // Call failed
-//         console.error('Failure', error)
-//       });
-//   },
-// }
 export default {
   initCall() {
+    const remoteAudio = document.getElementById('remoteAudio');
     $('.toratto-call').click(function(e) {
       e.preventDefault();
-      const simpleUser = new Web.SimpleUser(server, options);
+      // Construct a SimpleUser instance
+      simpleUser = new Web.SimpleUser(server, {
+        aor: 'sip:124@pbx.grupotoratto.com', // caller
+        media: {
+          constraints: { audio: true, video: false }, // audio only call
+          remote: { audio: remoteAudio }, // play remote audio
+        },
+        userAgentOptions: {
+          authorizationPassword: 'gt123456',
+          authorizationUsername: '124',
+        },
+      });
       simpleUser.connect()
       .then(() => {
         $('.icon-wrapper-call').hide();
         $('.icon-wrapper-hangup').show();
         simpleUser.register();
-        simpleUser.call('sip:100@pbx.cinusual.com');
+        simpleUser.call('sip:126@pbx.grupotoratto.com');
       })
       .catch((error) => {
         // Call failed
@@ -65,7 +40,7 @@ export default {
   initHanup() {
     $('.toratto-hangup').click(function(e) {
       e.preventDefault();
-/*
+
       simpleUser.hangup()
       .then(() => {
         $('.icon-wrapper-hangup').hide();
@@ -75,7 +50,6 @@ export default {
         // Call failed
         console.error('Failure', error)
       });
-      */
     });
   },
 }
