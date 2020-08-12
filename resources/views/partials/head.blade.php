75c4fe296eb8288d8185c3abe19d00ac00b404f7
Víctor Valencia <valencia6x@gmail.com>
2020-08-12 12:53:22 -0500

    google analitycs code

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,4 +3,12 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175256411-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-175256411-1');
+  </script>
 </head>
