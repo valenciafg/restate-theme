730a7a9b62c3f462832bf8e61cb88f1112f48526
Víctor Valencia <valencia6x@gmail.com>
2022-09-04 08:47:30 -0500

    popup single res

--- resources/views/partials/res_project/popup.blade.php
+++ resources/views/partials/res_project/popup.blade.php
@@ -1,9 +1,8 @@
 @php
   $show_popup = $project['show_popup'];
   $popup_image = $project['popup_image'];
+  $url = '#';
 @endphp
 @if (!empty($show_popup) && $show_popup !== 'FALSE')
-<a id="main-popup" href="{{$popup_image}}" data-fancybox="gallery" style="display: none;">
-	<img src="{{$popup_image}}" alt="popup"/>
-</a>
+<div id="main-popup-data" data-img="{{$popup_image}}" data-url="{{$url}}" style="display: none;"></div>
 @endif
